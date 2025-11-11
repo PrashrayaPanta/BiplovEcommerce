32136b57cbbc65ca9f8c83ef2ed400660f603f51
Prashraya Panta <prashrayapanta33@gmail.com>
2025-11-11 23:41:41 +0545

    Improvement: added Loading Component in BlogPost

--- EcommerceClient/src/components/LoadingComponent.jsx
+++ EcommerceClient/src/components/LoadingComponent.jsx
@@ -1,11 +1,9 @@
-
-
 const LoadingComponent = () => {
   return (
-    <div className="flex justify-center bg-red-400 items-center">
-        <i className="fa-solid fa-refresh fa-spin"></i>
+    <div className="flex justify-center  items-center h-80">
+      <i className="fa-solid fa-refresh fa-spin"></i>
     </div>
-  )
-}
+  );
+};
 
-export default LoadingComponent
+export default LoadingComponent;
--- EcommerceClient/src/pages/Customer/Blog/BlogPost.jsx
+++ EcommerceClient/src/pages/Customer/Blog/BlogPost.jsx
@@ -4,13 +4,21 @@ import BlogBox from "../../../components/blogComponents/BlogBox"; // Adjust path
 import WriteBlog from "@/components/blogComponents/WriteBlog";
 import http from "@/http";
 import { dtFormat } from "@/library";
+import LoadingComponent from "@/components/LoadingComponent";
 
 export function BlogPost() {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const getAllPost = async () => {
-    const { data } = await http.get("/post");
-    setPosts(data.posts);
+    try {
+      setLoading(true);
+      const { data } = await http.get("/post");
+      setPosts(data.posts);
+    } catch (error) {
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -23,19 +31,23 @@ export function BlogPost() {
     <div className="container mx-auto py-8 mt-20">
       {/* <WriteBlog /> */}
       <h2 className="text-3xl font-bold mb-6">Our Blog</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {posts.map((blog, index) => (
-          <BlogBox
-            key={index}
-            content={blog.content}
-            title={blog.title}
-            tags={blog.tags}
-            date={dtFormat(blog.createdAt)}
-            slug={blog.slug}
-            description={blog.content}
-          />
-        ))}
-      </div>
+      {loading ? (
+        <LoadingComponent />
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 bg-slate-100">
+          {posts.map((blog, index) => (
+            <BlogBox
+              key={index}
+              content={blog.content}
+              title={blog.title}
+              tags={blog.tags}
+              date={dtFormat(blog.createdAt)}
+              slug={blog.slug}
+              description={blog.content}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
